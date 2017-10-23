439db310efd2d20e04c148edabc5bcd656043879
Flavio Heleno <flaviohbatista@gmail.com>
2017-10-23 20:28:33 -0200

    Initial Commit

---
+++ StoryboardMerger/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StoryboardMerger
+//
+//  Created by Flavio Heleno on 23/10/17.
+//  Copyright © 2017 Flavio Heleno. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
